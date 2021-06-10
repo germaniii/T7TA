cf3f9ab6eb630bb68daf4f2b9f40292dd68740c9
German III <germaniiifelisarta@gmail.com>
2021-06-10 09:19:21 +0800

    v2.0.2 Pre Broadcast Receiver

---
+++ app/src/main/java/com/example/emav1/SerialBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.example.emav1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SerialBroadcastReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
